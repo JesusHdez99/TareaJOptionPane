3837161d39658869ae329227d7cf104690f94dc3
Jesus Hernandez <juan.hmontoya@alumnos.udg.mx>
2021-09-08 16:06:11 -0500

    updated

---
+++ src/main/java/Problema8.java
@@ -0,0 +1,16 @@
+
+/**Programa8
+ *Programa que imprima los numeros pares
+ *@author yisus
+ */
+import javax.swing.JOptionPane;
+public class Problema8 {
+
+public static void main(String args[])
+ {
+for(int i=0;i<=1000;i+=2)
+{
+JOptionPane.showMessageDialog(null,i);
+}
+ }
+}
